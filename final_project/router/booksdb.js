e8087be0f4c60111d4528debe6038174995e9402
ParkFedCU <98168889+ParkFedCU@users.noreply.github.com>
2025-03-31 15:10:46 -0400

    Update booksdb.js

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,15 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {},"isbn":"000000001" },
+      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {},"isbn":"000000002" },
+      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {},"isbn":"000000003" },
+      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {},"isbn":"000000004" },
+      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {},"isbn":"000000005" },
+      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {},"isbn":"000000006" },
+      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {},"isbn":"000000007" },
+      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {},"isbn":"000000008" },
+      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {},"isbn":"000000009" },
+      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {},"isbn":"000000010" }
 }
 
 module.exports=books;
+
