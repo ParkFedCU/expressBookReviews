95cd9572106d2d88b2993e1acfa34d69b7f092a8
ParkFedCU <98168889+ParkFedCU@users.noreply.github.com>
2025-04-01 15:10:53 -0400

    Update booksdb.js

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,14 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {},"isbn":"000000001" },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {},"isbn":"000000002" },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {},"isbn":"000000003" },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {},"isbn":"000000004" },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {},"isbn":"000000005" },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {},"isbn":"000000006" },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {},"isbn":"000000007" },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {},"isbn":"000000008" },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {},"isbn":"000000009" },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {},"isbn":"000000010" }
+      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {"from Mary:":"great book!"},"isbn":"000000001" },
+      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {"from anonymous:":"great book!"},"isbn":"000000002" },
+      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {"from Jill:":"great book!"},"isbn":"000000003" },
+      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {"from tim:":"great book!"},"isbn":"000000004" },
+      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {"from Sally:":"great book!"},"isbn":"000000005" },
+      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {"from Ellen:":"eh book!"},"isbn":"000000006" },
+      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {"from tim:":"great book!"},"isbn":"000000007" },
+      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {"from Steve:":"boring book!"},"isbn":"000000008" },
+      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {"from Jon:":"great book!"},"isbn":"000000009" },
+      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {"from Luke:":"great book!"},"isbn":"000000010" }
 }
 
 module.exports=books;
